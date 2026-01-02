91d0f50d3364bf43ef53d29a02949a59729ba99f
armanisteaqali10-jpg <armanisteaqali10@gmail.com>
2026-01-02 09:59:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const options = document.querySelectorAll(".option");
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+
+        // 1️⃣ Remove selected class from all options
+        options.forEach(opt => opt.classList.remove("selected"));
+
+        // 2️⃣ Add selected class to clicked option
+        option.classList.add("selected");
+
+        // 3️⃣ Log selected option (for testing)
+        console.log("Selected:", option.dataset.option);
+    });
+});
 

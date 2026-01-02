f260848db2268ff5979a6dbc3457de29490f67a2
armanisteaqali10-jpg <armanisteaqali10@gmail.com>
2026-01-02 10:44:34 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,9 +1,20 @@
 const questions = [
-    {
-        image: "images/q1.png"
-    },
-    {
-        image: "images/q2.png"
-    }
+  {
+    id: 1,
+    image: "images/q1.png",
+    options: ["A", "B", "C", "D"],
+    correct: "A"
+  },
+  {
+    id: 2,
+    image: "images/q2.png",
+    options: ["A", "B", "C", "D"],
+    correct: "C"
+  },
+  {
+    id: 3,
+    image: "images/q3.png",
+    options: ["A", "B", "C", "D"],
+    correct: "B"
+  }
 ];
-

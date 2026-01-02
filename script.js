92120fb011dd29703a5b9dcb70d05bc5fa9a080d
armanisteaqali10-jpg <armanisteaqali10@gmail.com>
2026-01-02 10:46:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+let currentQuestionIndex = 0;
+let selectedAnswer = null;
+
+const questionImage = document.getElementById("question-image");
 const options = document.querySelectorAll(".option");
+const nextBtn = document.getElementById("next-btn");
+const title = document.getElementById("question-title");
 
-options.forEach(option => {
-    option.addEventListener("click", () => {
+function loadQuestion() {
+  const q = questions[currentQuestionIndex];
 
-        // Remove previous selection
-        options.forEach(opt => opt.classList.remove("selected"));
+  title.textContent = `NEET CBT – Question ${q.id}`;
+  questionImage.src = q.image;
 
-        // Select clicked option
-        option.classList.add("selected");
+  selectedAnswer = null;
+  options.forEach(btn => {
+    btn.classList.remove("selected");
+  });
+}
 
-        console.log("Selected:", option.dataset.option);
-    });
+options.forEach(button => {
+  button.addEventListener("click", () => {
+    options.forEach(btn => btn.classList.remove("selected"));
+    button.classList.add("selected");
+    selectedAnswer = button.dataset.option;
+  });
 });
 
+nextBtn.addEventListener("click", () => {
+  if (selectedAnswer === null) {
+    alert("Please select an option");
+    return;
+  }
 
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex >= questions.length) {
+    alert("Test completed!");
+    return;
+  }
+
+  loadQuestion();
+});
+
+loadQuestion();
